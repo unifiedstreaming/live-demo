16815ded7573ff81d24df311506f4cde9722a2ad
Mark Ogle <mark@unified-streaming.com>
2020-06-05 16:09:11 +0200

    video working but audio glitches on encoder join/leave

--- ffmpeg/entrypoint.sh
+++ ffmpeg/entrypoint.sh
@@ -25,20 +25,29 @@ if [ -z "${PUB_POINT_URI}" ]; then
 fi
 
 # get current time in microseconds
-DATE_MICRO=$(LANG=C date +%s.%6N)
-DATE_PART1=${DATE_MICRO%.*}
-DATE_PART2=${DATE_MICRO#*.}
+# DATE_MICRO=$(LANG=C date +%s.%6N)
+# DATE_PART1=${DATE_MICRO%.*}
+# DATE_PART2=${DATE_MICRO#*.}
+DATE_MICRO=$(($(LANG=C date +%s%6N)/960000*960000))
+DATE_PART1=$((${DATE_MICRO}/1000000))
+DATE_PART2=$((${DATE_MICRO}%1000000))
+DATE_MS_PART=$(($DATE_PART2/1000))
 # the -ism_offset option has a timescale of 10,000,000, so add an extra zero
-ISM_OFFSET=${DATE_PART1}${DATE_PART2}0
+ISM_OFFSET=${DATE_MICRO}0
 # the number of seconds into the current day
 DATE_MOD_DAYS=$((${DATE_PART1}%86400))
+
+echo DATE_MICRO=$DATE_MICRO
+echo DATE_PART1=$DATE_PART1
+echo DATE_PART2=$DATE_PART2
+echo ISM_OFFSET=$ISM_OFFSET
+echo DATE_MOD_DAYS=$DATE_MOD_DAYS
 
 set -x
 exec ffmpeg \
   -re \
   -nostats \
   -f lavfi -i smptehdbars=size=1280x720 \
-  -f lavfi -i anullsrc \
   ${LOGO_OVERLAY} \
   -filter_complex "\
     drawtext=\
@@ -49,20 +58,31 @@ exec ffmpeg \
       fontsize=32:\
       text='%{pts\:gmtime\:${DATE_PART1}\:%Y-%m-%d}%{pts\:hms\:${DATE_MOD_DAYS}.${DATE_PART2}}':\
       x=(w-tw)/2:\
-      y=30\
+      y=30;\
+    sine=frequency=1:beep_factor=480:sample_rate=48000, \
+      atempo=0.5[a1]; \
+    sine=frequency=1:beep_factor=960:sample_rate=48000, \
+      atempo=0.5, \
+      adelay=1000[a2]; \
+    [a1][a2]amix, \
+    highpass=40,
+    adelay=${DATE_MS_PART}[a] \
       " \
   -g ${GOP_LENGTH} \
   -r ${FRAME_RATE} \
   -keyint_min ${GOP_LENGTH} \
   -c:v libx264 \
-  -preset veryfast \
+  -preset ultrafast \
+  -tune zerolatency \
   -profile:v baseline \
+  -b:v 500k \
+  -ar 48000 \
   -c:a aac \
   -map 0:v \
-  -map 1:a \
+  -map "[a]" \
   -fflags +genpts \
-  -movflags isml+frag_keyframe \
-  -write_prft pts \
+  -movflags isml+frag_every_frame+delay_moov+cmaf+negative_cts_offsets+separate_moof \
   -ism_offset ${ISM_OFFSET} \
+  -write_prft pts \
   -f ismv \
   ${PUB_POINT_URI}
